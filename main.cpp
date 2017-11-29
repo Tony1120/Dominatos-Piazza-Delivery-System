a33d351098ee057b0e13ceb59fbe81436e071d82
Nick Hunter <nhunter20@wooster.edu>
2017-11-29 13:41:29 -0500

    Initial main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Jemal, Nick, Tony
+//PizzaHub
+
+#include <iostream>
+//#include 'pizza.h'
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World" << endl;
+    return 0;
+}
