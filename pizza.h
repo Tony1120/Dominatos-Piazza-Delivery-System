f73a9f70a41c57d174a470788523b99b0859b710
jemalmahdi <jemalmahdi@gmail.com>
2017-12-03 19:30:03 -0500

    Update pizza.h

--- pizza.h
+++ pizza.h
@@ -20,23 +20,26 @@
 using namespace std;
 class Time{
 public:
-    
+    //Pre: None
     //Post: Creates a time with default values for data members.
     Time();
-    
+
     //Pre: 0 <= hour <= 23 and 0 <= min <= 59.
     //Post: Creates a time with the given hour and minute.
     Time(int hour, int min) throw (logic_error);
-    
+
+    //Pre: None
     //Post: Returns the difference in minutes between t1 and t2. Assumes t2 is between 00:00 and 23:59 hours after t1.
     static int elapsedMin(Time t1, Time t2);
-    
+
+    //Pre: Non
     //Post: Returns a string containing the hour and minute (e.g., “13:01”).
     string toString();
-    
+
 private:
-    int hour;
-    int minutes;
+  int hour;
+  int minutes;
+  int curr_time_min;			// Tony requested for this data member
 };
 
 
