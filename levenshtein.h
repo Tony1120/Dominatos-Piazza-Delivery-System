eabcfb616daee0ac553f76cfc527170f7f95317f
Nick Hunter <nhunter20@wooster.edu>
2017-12-06 21:03:49 -0500

    Things now function

---
+++ levenshtein.h
@@ -0,0 +1,23 @@
+//
+//  levenshtein.hpp
+//  autocorrect
+//
+//  Created by Nicholas Hunter on 12/6/17.
+//  Copyright © 2017 College of Wooster. All rights reserved.
+//
+
+#ifndef levenshtein_hpp
+#define levenshtein_hpp
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+unsigned int levenshtein_distance(const std::string& s1, const std::string& s2);
+string guessCommand(string word);
+bool isValidCommand(string word);
+
+#endif /* levenshtein_hpp */
