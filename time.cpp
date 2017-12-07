fdb7cc363e211bab4faedfae3f5c71ca94f3eaf7
jemalmahdi <jemalmahdi@gmail.com>
2017-12-07 11:55:55 -0500

    Add files via upload
    Time class with comments

--- time.cpp
+++ time.cpp
@@ -1,18 +1,30 @@
+//--------------------------------------------------------------------
+//   Jemal, Nick, Tony
+//   PizzaHub
+//
+//  Final Project                                           time.cpp
+//
+//  Implementation for the time class
+//
+//--------------------------------------------------------------------
+
 #include "time.h"
 
 using namespace std;
 
-//Pre: None
-//Post: Creates a time with default values for data members.
+//Default constructor
+//Precondition: None
+//Postcondition: Creates a time with default values for data members.
 Time::Time() {
   hour = 0;
   minutes = 0;
   curr_time_min = (hour * 60) + minutes;
 }
 
-//Pre: 0 <= hour <= 23 and 0 <= min <= 59.
-//Post: Creates a time with the given hour and minute.
-Time::Time(int hour, int min) throw (logic_error) {                   // the throw logic error isnt working, This might be a compiler problem
+//Parameterized constructor
+//Precondition: 0 <= hour <= 23 and 0 <= min <= 59.
+//Postcondition: Creates a time with the given hour and minute.
+Time::Time(int hour, int min) throw (logic_error) {                   
   if(hour<0 || hour>=23){
     throw logic_error("Hour has to be between 0 and 23");
   }
@@ -25,15 +37,15 @@ Time::Time(int hour, int min) throw (logic_error) {                   // the thr
 }
 
 //Assignment operator
-//Pre: None
-//Assigns other to a Time object
+//Precondition: None
+//Postcondition: Assigns other to a Time object
 void Time::operator = ( const Time &other){
     hour = other.hour;
     minutes = other.minutes;
 }
 
-//Pre: None
-//Post: Returns the difference in minutes between t1 and t2. Assumes t2 is between 00:00 and 23:59 hours after t1.
+//Precondition: None
+//Postcondition: Returns the difference in minutes between t1 and t2. Assumes t2 is between 00:00 and 23:59 hours after t1.
 int Time::elapsedMin(Time t1, Time t2){
   int min_elasped;
   min_elasped = (t2.hour * 60 + t2.minutes)-(t1.hour * 60 + t1.minutes);
@@ -43,8 +55,8 @@ int Time::elapsedMin(Time t1, Time t2){
   return min_elasped;
 }
 
-//Pre: Non
-//Post: Returns a string containing the hour and minute (e.g., “13:01”).
+//Precondition: Non
+//Postcondition: Returns a string containing the hour and minute (e.g., “13:01”).
 string Time::toString(){
     string the_time;
     string zero_pad = "";
--- time.h
+++ time.h
@@ -1,3 +1,13 @@
+//--------------------------------------------------------------------
+//   Jemal, Nick, Tony
+//   PizzaHub
+//
+//  Final Project                                           time.h
+//
+//  Declaration for the time class
+//
+//--------------------------------------------------------------------
+
 #ifndef TIME_H
 #define TIME_H
 
@@ -9,31 +19,34 @@
 using namespace std;
 class Time{
 public:
-    //Pre: None
-    //Post: Creates a time with default values for data members.
+
+	//Default constructor
+    //Precondition: None
+    //Postcondition: Creates a time with default values for data members.
     Time();
 
-    //Pre: 0 <= hour <= 23 and 0 <= min <= 59.
-    //Post: Creates a time with the given hour and minute.
+	//Parameterized constructor 
+    //Precondition: 0 <= hour <= 23 and 0 <= min <= 59.
+    //Postcondition: Creates a time with the given hour and minute.
     Time(int hour, int min) throw (logic_error);
 
     //Assignment operator
-    //Pre: None
-    //Assigns other to a Time object
+    //Precondition: None
+    //Postcondition: Assigns other to a Time object
     void operator = ( const Time &other);
 
     
-    //Pre: None
-    //Post: Returns the difference in minutes between t1 and t2. Assumes t2 is between 00:00 and 23:59 hours after t1.
+    //Precondition: None
+    //Postcondition: Returns the difference in minutes between t1 and t2. Assumes t2 is between 00:00 and 23:59 hours after t1.
     static int elapsedMin(Time t1, Time t2);
 
-    //Pre: Non
-    //Post: Returns a string containing the hour and minute (e.g., “13:01”).
+    //Precondition: Non
+    //Postcondition: Returns a string containing the hour and minute (e.g., “13:01”).
     string toString();
 
 private:
   int hour;
   int minutes;
-  int curr_time_min;			// Tony requested for this data member
+  int curr_time_min;			// current time in minutes
 };
 #endif
